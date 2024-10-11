2282cfbfdf35d706c980bee6d816e258dfbbf93b
Alex Culeva <culevalex@gmail.com>
2024-10-11 16:20:05 +0300

    Add Junior devs careers

--- apps/tnf-landing/src/lib/data/careers.ts
+++ apps/tnf-landing/src/lib/data/careers.ts
@@ -1,9 +1,11 @@
-import Img from '../../assets/technologies/swift.png';
+import React from '../../assets/technologies/react.png';
+import Swift from '../../assets/technologies/swift.png';
+import Kotlin from '../../assets/technologies/kotlin.png';
 
 export const careers = {
   vacancies: [
     {
-      image: Img,
+      image: React,
       title: 'React Native Engineer',
       tags: ['Mobile', 'Full-time', 'Hybrid'],
       weAreLookingFor: {
@@ -70,6 +72,122 @@ export const careers = {
       ],
       applicationUrl:
         'https://airtable.com/appwCNmdlYLUzKCTk/shr9351NZHQSnQ5rL',
+    },
+    {
+      image: Swift,
+      title: 'Junior iOS Developer',
+      tags: ['Mobile', 'Full-time', 'Hybrid'],
+      weAreLookingFor: {
+        title: 'Role Description',
+        body: "If you like Native mobile development, and want to grow into a strong iOS Developer who can take an app from creating a new project in Xcode to releasing it on the App Store, this is the role for you.",
+      },
+      jobProperties: [
+        {
+          title: 'Location',
+          value: 'Chisinau',
+        },
+        {
+          title: 'Job Type',
+          value: 'Full-time',
+        },
+        {
+          title: 'Department',
+          value: 'Mobile',
+        },
+        {
+          title: 'Work format',
+          value: 'Hybrid',
+        },
+      ],
+      lists: [
+        {
+          title: 'Responsibilities',
+          items: [
+            'Developing mobile apps in Swift, SwiftUI and rarely UIKit',
+            'Close collaboration with our designer and the rest of the mobile team to provide seamless UX',
+            'Deploying apps to TestFlight and AppStore',
+            'Working on in-house apps that target the moldovan market'
+          ],
+        },
+        {
+          title: 'Qualifications',
+          items: [
+            'Strong desire to GROW and learn how to build beautiful apps',
+            'Some prior experience with Swift and programming in general',
+            'Intermediate level of English',
+          ],
+        },
+        {
+          title: 'What we offer',
+          items: [
+            'Mobile projects that you won\'t find anywhere else on the Moldovan market',
+            'A very strong Native team to help you grow and improve',
+            'Competitive salary',
+            'Office in the city center',
+            'Hybrid work schedule',
+            'Flexible working hours',
+          ],
+        },
+      ],
+      applicationUrl: 'https://airtable.com/appaWFNysvwFR83PE/shryE6A5L4kbihp5l',
+    },
+    {
+      image: Kotlin,
+      title: 'Junior Android Developer',
+      tags: ['Mobile', 'Full-time', 'Hybrid'],
+      weAreLookingFor: {
+        title: 'Role Description',
+        body: "If you like Native mobile development, and want to grow into a strong Android Developer who can take an app from creating a new project in Android Studio to releasing it on Google Play, this is the role for you.",
+      },
+      jobProperties: [
+        {
+          title: 'Location',
+          value: 'Chisinau',
+        },
+        {
+          title: 'Job Type',
+          value: 'Full-time',
+        },
+        {
+          title: 'Department',
+          value: 'Mobile',
+        },
+        {
+          title: 'Work format',
+          value: 'Hybrid',
+        },
+      ],
+      lists: [
+        {
+          title: 'Responsibilities',
+          items: [
+            'Developing mobile apps in Kotlin, Jetpack Compose and rarely XML',
+            'Close collaboration with our designer and the rest of the mobile team to provide seamless UX',
+            'Deploying apps to Google Play',
+            'Working on in-house apps that target the moldovan market'
+          ],
+        },
+        {
+          title: 'Qualifications',
+          items: [
+            'Strong desire to GROW and learn how to build beautiful apps',
+            'Some prior experience with Kotlin and programming in general',
+            'Intermediate level of English',
+          ],
+        },
+        {
+          title: 'What we offer',
+          items: [
+            'Mobile projects that you won\'t find anywhere else on the Moldovan market',
+            'A very strong Native team to help you grow and improve',
+            'Competitive salary',
+            'Office in the city center',
+            'Hybrid work schedule',
+            'Flexible working hours',
+          ],
+        },
+      ],
+      applicationUrl: 'https://airtable.com/appy1m9ZVxqPIZd1G/shrN5BQ8sd22f1LIJ',
     },
   ],
 };
