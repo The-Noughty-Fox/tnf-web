b5eb9094812059b37019671cb535918200ed44d2
Serghei Tibulschi <serghei@thenoughtyfox.com>
2024-12-08 12:15:34 +0200

    fix header links

--- apps/tnf-blog/src/components/header.tsx
+++ apps/tnf-blog/src/components/header.tsx
@@ -3,7 +3,8 @@ import { pageRoutes } from '../lib/pageRoutes.ts';
 
 const links = [
   { name: pageRoutes.services.label, href: pageRoutes.services.href },
-  { name: pageRoutes.portfolio.label, href: pageRoutes.services.href },
+  { name: pageRoutes.portfolio.label, href: pageRoutes.portfolio.href },
+  { name: pageRoutes.careers.label, href: pageRoutes.careers.href },
   { name: pageRoutes.contacts.label, href: pageRoutes.contacts.href },
   { name: pageRoutes.blog.label, href: pageRoutes.blog.href },
 ];
